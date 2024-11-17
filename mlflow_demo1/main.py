925570814a6563a2dcb34d7a90274670e69406b0
Sabina Zaman <sabina.zaman94@gmail.com>
2024-11-17 14:10:26 +0100

    multiple experiments with different algo in one program

--- mlflow_demo1/main.py
+++ mlflow_demo1/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import ElasticNet, Ridge, Lasso
 import mlflow
 import mlflow.sklearn
 from pathlib import Path
@@ -52,7 +52,10 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri(uri="")
 
     print("The set tracking uri is ", mlflow.get_tracking_uri())
-    exp = mlflow.set_experiment(experiment_name="experiment_5")
+
+###########First Experiment Elastic Net #############
+    print("First Experiment Elastic Net")
+    exp = mlflow.set_experiment(experiment_name="exp_multi_EL")
 
     print("Name: {}".format(exp.name))
     print("Experiment_id: {}".format(exp.experiment_id))
@@ -179,6 +182,294 @@ if __name__ == "__main__":
     params = {
         "alpha": 0.4,
         "l1_ratio": 0.4
+    }
+    mlflow.log_params(params)
+    # log metrics
+    metrics = {
+        "rmse": rmse,
+        "r2": r2,
+        "mae": mae
+    }
+    mlflow.log_metrics(metrics)
+    # log model
+    mlflow.sklearn.log_model(lr, "my_new_model_1")
+    mlflow.log_artifacts("data/")
+
+    artifacts_uri = mlflow.get_artifact_uri()
+    print("The artifact path is", artifacts_uri)
+
+    mlflow.end_run()
+
+    ###########Second Experiment Ridge #############
+    print("Second Experiment Ridge")
+    exp = mlflow.set_experiment(experiment_name="exp_multi_Ridge")
+
+    print("Name: {}".format(exp.name))
+    print("Experiment_id: {}".format(exp.experiment_id))
+    # print("Artifact Location: {}".format(exp.artifact_location))
+    # print("Tags: {}".format(exp.tags))
+    # print("Lifecycle_stage: {}".format(exp.lifecycle_stage))
+    # print("Creation timestamp: {}".format(exp.creation_time))
+
+    mlflow.start_run(run_name="run1.1")
+    tags = {
+        "engineering": "ML platform",
+        "release.candidate": "RC1",
+        "release.version": "2.0"
+    }
+
+    mlflow.set_tags(tags)
+
+    current_run = mlflow.active_run()
+    print("Active run id is {}".format(current_run.info.run_id))
+    print("Active run name is {}".format(current_run.info.run_name))
+
+    lr = Ridge(alpha=alpha,  random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Ridge (alpha={:f}".format(alpha))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    # log parameters
+    params = {
+        "alpha": alpha
+    }
+    mlflow.log_params(params)
+    # log metrics
+    metrics = {
+        "rmse": rmse,
+        "r2": r2,
+        "mae": mae
+    }
+    mlflow.log_metrics(metrics)
+    # log model
+    mlflow.sklearn.log_model(lr, "my_new_model_1")
+    mlflow.log_artifacts("data/")
+
+    artifacts_uri = mlflow.get_artifact_uri()
+    print("The artifact path is", artifacts_uri)
+
+    mlflow.end_run()
+    ########### Second run ######################
+    mlflow.start_run(run_name="run2.1")
+    tags = {
+        "engineering": "ML platform",
+        "release.candidate": "RC1",
+        "release.version": "2.0"
+    }
+
+    mlflow.set_tags(tags)
+
+    current_run = mlflow.active_run()
+    print("Active run id is {}".format(current_run.info.run_id))
+    print("Active run name is {}".format(current_run.info.run_name))
+
+    lr = Ridge(alpha=0.9, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Ridge (alpha={:f}".format(0.9))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    # log parameters
+    params = {
+        "alpha": 0.9
+    }
+    mlflow.log_params(params)
+    # log metrics
+    metrics = {
+        "rmse": rmse,
+        "r2": r2,
+        "mae": mae
+    }
+    mlflow.log_metrics(metrics)
+    # log model
+    mlflow.sklearn.log_model(lr, "my_new_model_1")
+    mlflow.log_artifacts("data/")
+
+    artifacts_uri = mlflow.get_artifact_uri()
+    print("The artifact path is", artifacts_uri)
+
+    mlflow.end_run()
+    ########### Third run ######################
+
+    mlflow.start_run(run_name="run3.1")
+    tags = {
+        "engineering": "ML platform",
+        "release.candidate": "RC1",
+        "release.version": "2.0"
+    }
+
+    mlflow.set_tags(tags)
+
+    current_run = mlflow.active_run()
+    print("Active run id is {}".format(current_run.info.run_id))
+    print("Active run name is {}".format(current_run.info.run_name))
+
+    lr = Ridge(alpha=0.4, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Ridge (alpha={:f}".format(0.4))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    # log parameters
+    params = {
+        "alpha": 0.4
+    }
+    mlflow.log_params(params)
+    # log metrics
+    metrics = {
+        "rmse": rmse,
+        "r2": r2,
+        "mae": mae
+    }
+    mlflow.log_metrics(metrics)
+    # log model
+    mlflow.sklearn.log_model(lr, "my_new_model_1")
+    mlflow.log_artifacts("data/")
+
+    artifacts_uri = mlflow.get_artifact_uri()
+    print("The artifact path is", artifacts_uri)
+
+    mlflow.end_run()
+
+    ###########Third Experiment Ridge #############
+    print("Third Experiment Ridge")
+    exp = mlflow.set_experiment(experiment_name="exp_multi_Lasso")
+
+    print("Name: {}".format(exp.name))
+    print("Experiment_id: {}".format(exp.experiment_id))
+    # print("Artifact Location: {}".format(exp.artifact_location))
+    # print("Tags: {}".format(exp.tags))
+    # print("Lifecycle_stage: {}".format(exp.lifecycle_stage))
+    # print("Creation timestamp: {}".format(exp.creation_time))
+
+    mlflow.start_run(run_name="run1.1")
+    tags = {
+        "engineering": "ML platform",
+        "release.candidate": "RC1",
+        "release.version": "2.0"
+    }
+
+    mlflow.set_tags(tags)
+
+    current_run = mlflow.active_run()
+    print("Active run id is {}".format(current_run.info.run_id))
+    print("Active run name is {}".format(current_run.info.run_name))
+
+    lr = Lasso(alpha=alpha,  random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Ridge (alpha={:f}".format(alpha))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    # log parameters
+    params = {
+        "alpha": alpha
+    }
+    mlflow.log_params(params)
+    # log metrics
+    metrics = {
+        "rmse": rmse,
+        "r2": r2,
+        "mae": mae
+    }
+    mlflow.log_metrics(metrics)
+    # log model
+    mlflow.sklearn.log_model(lr, "my_new_model_1")
+    mlflow.log_artifacts("data/")
+
+    artifacts_uri = mlflow.get_artifact_uri()
+    print("The artifact path is", artifacts_uri)
+
+    mlflow.end_run()
+    ########### Second run ######################
+    mlflow.start_run(run_name="run2.1")
+    tags = {
+        "engineering": "ML platform",
+        "release.candidate": "RC1",
+        "release.version": "2.0"
+    }
+
+    mlflow.set_tags(tags)
+
+    current_run = mlflow.active_run()
+    print("Active run id is {}".format(current_run.info.run_id))
+    print("Active run name is {}".format(current_run.info.run_name))
+
+    lr = Lasso(alpha=0.9, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Ridge (alpha={:f}".format(0.9))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    # log parameters
+    params = {
+        "alpha": 0.9
+    }
+    mlflow.log_params(params)
+    # log metrics
+    metrics = {
+        "rmse": rmse,
+        "r2": r2,
+        "mae": mae
+    }
+    mlflow.log_metrics(metrics)
+    # log model
+    mlflow.sklearn.log_model(lr, "my_new_model_1")
+    mlflow.log_artifacts("data/")
+
+    artifacts_uri = mlflow.get_artifact_uri()
+    print("The artifact path is", artifacts_uri)
+
+    mlflow.end_run()
+    ########### Third run ######################
+
+    mlflow.start_run(run_name="run3.1")
+    tags = {
+        "engineering": "ML platform",
+        "release.candidate": "RC1",
+        "release.version": "2.0"
+    }
+
+    mlflow.set_tags(tags)
+
+    current_run = mlflow.active_run()
+    print("Active run id is {}".format(current_run.info.run_id))
+    print("Active run name is {}".format(current_run.info.run_name))
+
+    lr = Lasso(alpha=0.4, random_state=42)
+    lr.fit(train_x, train_y)
+
+    predicted_qualities = lr.predict(test_x)
+    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+
+    print("Lasso (alpha={:f}".format(0.4))
+    print("  RMSE: %s" % rmse)
+    print("  MAE: %s" % mae)
+    print("  R2: %s" % r2)
+    # log parameters
+    params = {
+        "alpha": 0.4
     }
     mlflow.log_params(params)
     # log metrics
