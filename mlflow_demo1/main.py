3161418609840eda714ceef5efa93640fe228c1a
Sabina Zaman <sabina.zaman94@gmail.com>
2025-05-08 11:03:42 +0200

    Log Signature 1

--- mlflow_demo1/main.py
+++ mlflow_demo1/main.py
@@ -10,6 +10,8 @@ import mlflow
 import mlflow.sklearn
 from pathlib import Path
 import os
+from mlflow.models.signature import ModelSignature, infer_signature
+from mlflow.types.schema import Schema,ColSpec
 
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
@@ -33,10 +35,9 @@ if __name__ == "__main__":
     np.random.seed(40)
 
     # Read the wine-quality csv file from the URL
-    data = pd.read_csv("red-wine-quality.csv")
-    #os.mkdir("data/")
-    data.to_csv("data/red-wine-quality.csv", index=False)
-    # Split the data into training and test sets. (0.75, 0.25) split.
+    data = pd.read_csv(r'C:\Users\User\Documents\GitHub\-MLflowtoMLOps\mlflow_demo1\data\red-wine-quality.csv')
+
+   # os.mkdir("data/")
     train, test = train_test_split(data)
     train.to_csv("data/train.csv")
     test.to_csv("data/test.csv")
@@ -49,10 +50,10 @@ if __name__ == "__main__":
     alpha = args.alpha
     l1_ratio = args.l1_ratio
 
-    mlflow.set_tracking_uri(uri="http://127.0.0.1:5000")
+    mlflow.set_tracking_uri(uri="")
 
     print("The set tracking uri is ", mlflow.get_tracking_uri())
-    exp = mlflow.set_experiment(experiment_name="experiment_autolog")
+    exp = mlflow.set_experiment(experiment_name="experiment_signature")
     #get_exp = mlflow.get_experiment(exp_id)
 
     print("Name: {}".format(exp.name))
@@ -72,7 +73,9 @@ if __name__ == "__main__":
 
     mlflow.set_tags(tags)
     mlflow.sklearn.autolog(
-        log_input_examples=True
+        log_input_examples=False,
+        log_model_signatures=False,
+        log_models=False
     )
     lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
     lr.fit(train_x, train_y)
@@ -86,8 +89,44 @@ if __name__ == "__main__":
     print("  MAE: %s" % mae)
     print("  R2: %s" % r2)
 
-    mlflow.log_artifact("red-wine-quality.csv")
+    input_data = [
+        {"name": "fixed acidity", "type": "double"},
+        {"name": "volatile acidity", "type": "double"},
+        {"name": "citric acid", "type": "double"},
+        {"name": "residual sugar", "type": "double"},
+        {"name": "chlorides", "type": "double"},
+        {"name": "free sulfur dioxide", "type": "double"},
+        {"name": "total sulfur dioxide", "type": "double"},
+        {"name": "density", "type": "double"},
+        {"name": "pH", "type": "double"},
+        {"name": "sulphates", "type": "double"},
+        {"name": "alcohol", "type": "double"},
+        {"name": "quality", "type": "double"}
+    ]
 
+    output_data = [{'type': 'long'}]
+
+    input_schema = Schema([ColSpec(col["type"], col['name']) for col in input_data])
+    output_schema = Schema([ColSpec(col['type']) for col in output_data])
+
+    signature = ModelSignature(inputs=input_schema, outputs=output_schema)
+
+    input_example = {
+        "fixed acidity": np.array([7.2, 7.5, 7.0, 6.8, 6.9]),
+        "volatile acidity": np.array([0.35, 0.3, 0.28, 0.38, 0.25]),
+        "citric acid": np.array([0.45, 0.5, 0.55, 0.4, 0.42]),
+        "residual sugar": np.array([8.5, 9.0, 8.2, 7.8, 8.1]),
+        "chlorides": np.array([0.045, 0.04, 0.035, 0.05, 0.042]),
+        "free sulfur dioxide": np.array([30, 35, 40, 28, 32]),
+        "total sulfur dioxide": np.array([120, 125, 130, 115, 110]),
+        "density": np.array([0.997, 0.996, 0.995, 0.998, 0.994]),
+        "pH": np.array([3.2, 3.1, 3.0, 3.3, 3.2]),
+        "sulphates": np.array([0.65, 0.7, 0.68, 0.72, 0.62]),
+        "alcohol": np.array([9.2, 9.5, 9.0, 9.8, 9.4]),
+        "quality": np.array([6, 7, 6, 8, 7])
+    }
+    mlflow.log_artifact("C:/Users/User/Documents/GitHub/-MLflowtoMLOps/mlflow_demo1/data/red-wine-quality.csv")
+    mlflow.sklearn.log_model(lr, "model", signature=signature, input_example=input_example)
     artifacts_uri=mlflow.get_artifact_uri()
     print("The artifact path is",artifacts_uri )
     mlflow.end_run()
